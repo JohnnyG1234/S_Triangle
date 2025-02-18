f44fa58d3d45140f0d44f76210029d7792c46d21
John Groton <john.groton@mymail.champlain.edu>
2025-02-18 17:04:12 -0500

    got svg part working

--- Sources/SVGLibrary/SVG.swift
+++ Sources/SVGLibrary/SVG.swift
@@ -9,17 +9,27 @@ import Foundation
 
 open class SVG {
     // YOUR CODE HERE
+    private var svgString: String =  ""
+    private let fileManager = FileManager.default
     
     // Initialize the SVG file with commands that will create a
     // width x height canvas
     public init(width: UInt, height: UInt) {
-        // YOUR CODE HERE
+        svgString += "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+        svgString += "<svg version=\"1.1\" baseProfile=\"full\" width=\"\(width)\" height=\"\(height)\" xmlns=\"http://www.w3.org/2000/svg\">\n"
     }
     
     // YOUR CODE HERE
     
     // Write the SVG file to disk
     public func write(filePath: String) {
-        // YOUR CODE HERE
+        let svgData: Data? = svgString.data(using: .utf8) 
+
+        let file = fileManager.createFile(atPath: filePath, contents: svgData, attributes: nil)
+        if file  {
+            print("File created.") 
+        } else  {
+            print("Failed to create file.")
+        }
     }
 }
